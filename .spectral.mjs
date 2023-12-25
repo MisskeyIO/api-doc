a0e8bf8694940e174d3ba20c06eac643f8ba76c8
村上さん <murakami@misskey.io>
2023-12-25 15:30:59 +0900

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzMDUwNg/spectral.js?branch_slug=master&token=a2659e2a-a5b2-4c98-ac6b-c95c7d8a0967";
+export default { extends: ruleset };
